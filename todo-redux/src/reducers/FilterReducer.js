2262832b6b9ed8dd831feb4203e6c44c5965abeb
Thong <thong@inspectorio.com>
2017-10-09 21:58:05 +0700

    done fileter

--- todo-redux/src/reducers/FilterReducer.js
+++ todo-redux/src/reducers/FilterReducer.js
@@ -1,11 +1,12 @@
 const FilterReducer = (state = 'FILTER_ALL', action) => {
+    console.log("action type", action.type);
     switch (action.type) {
         case 'FILTER_COMPLETED':
-            return Object.assign({}, state, {filter: 'FILTER_COMPLETED'});
+            return 'FILTER_COMPLETED'
         case 'FILTER_NEW':
-            return Object.assign({}, state, {filter: 'FILTER_NEW'});
+            return 'FILTER_NEW'
         case 'FILTER_ALL':
-            return Object.assign({}, state, {filter: 'FILTER_ALL'});
+            return 'FILTER_ALL'
         default:
             return state
     }
